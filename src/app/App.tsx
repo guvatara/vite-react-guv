fad732c4817f3e3e923e831ff9a63ba79ae51136
Guvatara <guvatara@gmail.com>
2025-01-02 18:48:42 +0300

    fix App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Button from 'react-bootstrap/Button';
-import reactLogo from "./assets/react.svg";
+import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
