0308ca8bb5603d2994395b6dc0fdcf5ef55d1f64
Guvatara <guvatara@gmail.com>
2025-01-03 18:41:09 +0300

    refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,40 +1,14 @@
-import { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css";
-
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Main } from "../pages/main/main";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button variant="success" onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>
-          Edite <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
